e15a83e44e83f14492e5867fd0748b27426d528a
Jeras Bertine <jeras.bertine@epitech.eu>
2023-04-06 17:29:02 +0200

    feat

--- Workshop 1/src/Experience.js
+++ Workshop 1/src/Experience.js
@@ -1,5 +1,18 @@
-export default function Experience()
-{
-    return <>
+import React from 'react';
+import { useFrame } from 'react-three-fiber';
+
+function Experience() {
+  return (
+    <>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+
+      <mesh>
+        <boxBufferGeometry />
+        <meshStandardMaterial color={'hotpink'} />
+      </mesh>
     </>
+  );
 }
+
+export default Experience;
